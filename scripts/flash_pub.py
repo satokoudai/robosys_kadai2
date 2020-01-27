36e3a4711183f925540dc715f722d0e747c08fd8
satokoudai <59693685+satokoudai@users.noreply.github.com>
2020-01-27 20:08:37 +0900

    Update flash_pub.py

--- scripts/flash_pub.py
+++ scripts/flash_pub.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+###送信プログラム###
+
+###ライブラリを持ってくる###
 import roslib
 import rospy
 import time
@@ -8,30 +11,32 @@ from std_msgs.msg import String
 
 #main
 if __name__ == '__main__':
-    ### init io port ###
+    ### 14(ラズパイの8番)と17（ラズパイの11番）の二つのピンを使えるようにする###
     subprocess.check_call('gpio export 14 out',shell=True)
     subprocess.check_call('gpio export 17 out',shell=True)
     ###
     rospy.init_node('led_pub')
 
     io = wiringpi.GPIO(wiringpi.GPIO.WPI_MODE_GPIO_SYS)
-    io.pinMode(14,io.OUTPUT)  # Setup pin 11
-    io.pinMode(17,io.OUTPUT)  # Setup pin 8
+    io.pinMode(14,io.OUTPUT)  # ピン番号８番に出力ピンと教える
+    io.pinMode(17,io.OUTPUT)  # ピン番号11番に出力ピンと教える
 
+   ###周り続ける
     while not rospy.is_shutdown():
-        i = raw_input()
+        i = raw_input()###入力を受け付ける。プログラムが入力待ちになる
         pub = rospy.Publisher('led_flash_str', String, queue_size=1)
         pub.publish(i)
 
         if i == '0' :
-            io.digitalWrite(14,0)
-            io.digitalWrite(17,0)
-            time.sleep(1)
+            io.digitalWrite(14,0) ###8番のピンの光を消す
+            io.digitalWrite(17,0)###11番のピンの光を消す
+            time.sleep(1)###スリープ
+            
         elif i == '1' :
-            io.digitalWrite(14,1)
-            io.digitalWrite(17,1)
-            time.sleep(1)
+            io.digitalWrite(14,1)###8番のピンを光らせる
+            io.digitalWrite(17,1)###11番のピンを光らせる
+            time.sleep(1)###スリープ
         else :
-            io.digitalWrite(14,0)
+            io.digitalWrite(14,0)###それ以外の時
             io.digitalWrite(17,0)
             time.sleep(1)
