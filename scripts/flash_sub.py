764060826b04e7c21ff5a2d8b548892ff0b145fd
satokoudai <59693685+satokoudai@users.noreply.github.com>
2020-01-27 20:12:44 +0900

    Update flash_sub.py

--- scripts/flash_sub.py
+++ scripts/flash_sub.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 ###受信するプログラム
 
-###
+###ライブラリを持ってくる
 import rospy
 from std_msgs.msg import String
 
 def string_check(msg):
-    if msg.data == '0' :
-        print("\rLED OFF")
-    elif msg.data == '1' :
-        print("\rLED ON")
+    if msg.data == '0' :###もし、0と入力されたら
+        print("\rLED OFF")###モニターに出力
+    elif msg.data == '1' :###もし、１と入力されたら
+        print("\rLED ON")###モニターに出力
 
 
 if __name__ == "__main__":
-    rospy.init_node("led_sub")
-    sub = rospy.Subscriber('led_flash_str', String, string_check)
+    rospy.init_node("led_sub")###ここで受け取る
+    sub = rospy.Subscriber('led_flash_str', String, string_check)###string_checkここで関数を呼ぶ
     rospy.spin()
